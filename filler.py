be460b89366c85a0fd75afe643232663f13aacd8
Michael Stewart <stewarmc@jmu.edu>
2018-12-14 00:11:55 -0500

    IT LIVES

--- filler.py
+++ filler.py
@@ -1,38 +1,20 @@
-# # filler.py
-
-# # Import the Canvas class
-# from canvasapi import Canvas
+# filler.py
 
 import os
 CANVAS_KEY = os.environ["CANVAS_KEY"]
 CANVAS_URL = "https://canvas.jmu.edu/"
 
-# if __name__ == "__main__":    
-
-#     # Initialize a new Canvas object
-#     canvas = Canvas(CANVAS_URL, CANVAS_KEY)
-#     # print(dir(canvas))
-#     # print(canvas.get_user(5480485))
-#     course = canvas.get_course(1584732)
-#     # for r in course.get_rubrics():
-#     #     print(r)
-#     print(dir(course.get_rubric(687653)))
-#     assg = course.get_assignment(9810070)
-#     submission = assg.get_submission(5533888)
-
-
 import sys
 import urllib
 import time
-# import urllib2
 import urllib.request
 import urllib.error
 from urllib.request import urlopen
 import urllib.parse
 
+import csv
+
 # https://stackoverflow.com/a/4014164/1449799
-
-
 def recursive_urlencode(d):
     """URL-encode a multidimensional dictionary.
 
@@ -75,34 +57,25 @@ class GradePoster(object):
         form_data = {"rubric_assessment":{}}
         self.url += "/{}"
         self.url = self.url.format(student_id)
+        print(self.url)
         for crit in crit_grade_comments:
             form_data['rubric_assessment'][crit]={
-                "points": crit_grade_comments[crit]["pts"],
-                "comments": crit_grade_comments[crit]["comment"]
+                "points": crit_grade_comments[crit]["points"]
             }
-            # print(crit, crit_grade_comments[crit]["pts"], crit_grade_comments[crit]["comment"])
-        # rubric_assessment = 'rubric_assessment[{}][points]'.format(crit)
-        # form_data[rubric_assessment] = pts
-        # grade_key = 'grade_data[{}][posted_grade]'.format(student_id)
-        # form_data[grade_key] = grade
-
-        # # if comment != "":
-        # #     comment_key = 'grade_data[{}][text_comment]'.format(student_id)
-        # #     form_data[comment_key] = comment
+            if "comment" in crit_grade_comments[crit] and len(crit_grade_comments[crit]["comment"]) > 0:
+                form_data['rubric_assessment'][crit]["comments"]: crit_grade_comments[crit]["comment"]
 
         form_data = recursive_urlencode(form_data)
         # print(form_data)
         data = str.encode(form_data)
-        # data = urllib.parse.urlencode(form_data).encode("utf-8")
-        # data = urllib.parse.urlencode(form_data)
-        # print(data)
+        print(data)
         header = {"Authorization": "Bearer {}".format(self.key)}
-        # print(header)
-        request = urllib.request.Request(self.url, data, headers=header)
-        # print(request.get_full_url())
-        # print(request.header_items())
+        request = urllib.request.Request(self.url, data, headers=header, method="PUT")
         attempt_count = 0
-        # print(request)
+
+        # "/api/v1/courses/:course_id/rubrics/:id"
+        # request = urllib.request.Request(
+        #     CANVAS_URL+"api/v1/courses/"+COURSE_ID+"/assignments/9810058/submissions/5533888?include[]=rubric_assessment", headers=header)
         while attempt_count < 4:
             try:
                 return urlopen(request)
@@ -114,37 +87,43 @@ class GradePoster(object):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("USAGE: python filler.py graded-with-rubric.csv")
+        sys.exit(1)
     COURSE_ID = "1584732"
-    ASSIGNMENT_ID = "9810070"
-    student_id = "5587357"
+    # ASSIGNMENT_ID = "9810070" #demo?
+    ASSIGNMENT_ID = "9810058"  # lab07
+    student_id = "5587357" #Test Student for CS 101
     gp = GradePoster(COURSE_ID, ASSIGNMENT_ID, CANVAS_KEY)
-    crit_grade_comments = {
-        "criterion__5412": {
-            "pts": 2,
-            "comment": ""
-        },
-        "criterion__3145": {
-            "pts": 2,
-            "comment": ""
-        },
-        "criterion__6838": {
-            "pts": 0.5,
-            "comment": ""
-        },
-        "criterion__4455": {
-            "pts": 2,
-            "comment": ""
-        },
-        "criterion__1269": {
-            "pts": 2,
-            "comment": ""
-        },
-        "criterion__8517": {
-            "pts": 0.5,
-            "comment": ""
-        }
-    }
+    with open(sys.argv[1], encoding='utf-8-sig', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+            sid = row["SID"]
+            crit_grade_comments = {}
+            for item in filter(lambda x: x != "SID", row):
+                crit_grade_comments[item] = {"points": row[item]}
+            response = gp.post_grade_update(student_id, crit_grade_comments)
+            print(response.read())
 
-    gp.post_grade_update(student_id, crit_grade_comments)
+    # crit_grade_comments = {
+    #     # these can come from the rubric page.  e.g. https://canvas.jmu.edu/courses/1584732/rubrics/687658
+    #     # inspect the table to get the criteria ids
+    #     # $(".criterion:not(.blank, #criterion_1)").map((i,e)=>e.id.substring("criterion_".length))
+    #     # "415008_7565": { 
+    #     #     "points": 1,
+    #     #     "comment": ""
+    #     # },
+    #     # "415008_2320": {
+    #     #     "points": 0,
+    #     # },
+    #     "415008_1553": {
+    #         "points": 2,
+    #     },
+    #     "415008_261": {
+    #         "points": 4,
+    #     }
+    # }
+
     # response = gp.post_grade_update(student_id, crit_grade_comments)
-    # print response.read()
+    # print(response.read())
